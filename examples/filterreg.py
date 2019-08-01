8c0510c280f9df31cf036277f653db7e55380cec
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2019-08-01 15:53:50 +0900

    add comment rote

--- examples/filterreg.py
+++ examples/filterreg.py
@@ -14,9 +14,10 @@ import sys
 
 sig = None
 ter = 1
-ol = 0.0001
+ol = 0.00010
 
-if __name__ == "__main__":
+
+def main():
 
     argvs = sys.argv
 
@@ -46,27 +47,38 @@ if __name__ == "__main__":
                 rot_in[j][i] = np.array(l_strip[k])
                 k=k+1
 
-    #print(rot_in)
-    source_t.transform(rot_in)
-
-    #o3.draw_geometries([source_t])
     source_t = o3.voxel_down_sample(source_t, voxel_size=0.020)
     source_t_plus_one = o3.voxel_down_sample(source_t_plus_one, voxel_size=0.020)
-    #o3.draw_geometries([source_t])
 
-    cbs = [callbacks.Open3dVisualizerCallback(source_t_plus_one, source_t)]
+    # 基準とするpointcloud, 回転させたいpointcloud の順番 
+    cbs = [callbacks.Open3dVisualizerCallback(source_t, source_t_plus_one)]
     objective_type = 'pt2pt'
     
-    tf_param, _, _ = filterreg.registration_filterreg(source_t_plus_one, source_t,
+    # 基準とするpointcloud, 回転させたいpointcloud の順番 
+    tf_param, _, _ = filterreg.registration_filterreg(source_t, source_t_plus_one, 
+                                                      source_t_plus_one.normals,
                                                       objective_type=objective_type,
                                                       sigma2=sig,
                                                       callbacks=cbs,
                                                       maxiter=ter,
                                                       tol=ol)
 
+    # rot =
+    #     | rot[00] rot[01] rot[02] t[0] |
+    #     | rot[10] rot[11] rot[12] t[1] |
+    #     | rot[20] rot[21] rot[22] t[2] |
+    #     |    0       0       0     1   |
+    #
+
     rot = trans.identity_matrix()
     rot[:3, :3] = tf_param.rot
 
+    rot[0][3] = tf_param.t[0]
+    rot[1][3] = tf_param.t[1]
+    rot[2][3] = tf_param.t[2]
+
+
+    print(rot)
 
     print("result: ", np.rad2deg(trans.euler_from_matrix(rot)),
           tf_param.scale, tf_param.t)
@@ -82,24 +94,11 @@ if __name__ == "__main__":
                 f.write(np.array2string(rot[j][i]) + "0" + "\n")
 
 
-
     result = copy.deepcopy(source_t_plus_one)
     result.points = tf_param.transform(result.points)
 
     # draw result
-    source_t.paint_uniform_color([1, 0, 0])
-    source_t_plus_one.paint_uniform_color([0, 1, 0])
-    result.paint_uniform_color([0, 0, 1])
 
-    #origin_t_plus_one = "/mnt/container-data/ply_data/" + tf_cloud
-    #P_t_plus_one = o3.read_point_cloud(origin_t_plus_one)
-    
-    #P_t_plus_one.points = tf_param.transform(P_t_plus_one.points)
-    
-    #result_name = "/mnt/container-data/num_data/" + tf_cloud
-    #o3.io.write_point_cloud(result_name, P_t_plus_one)
+if __name__ == "__main__":
 
-
-    #o3.draw_geometries([source_t, source_t_plus_one, P_t_plus_one])
-
-    
+    main()
